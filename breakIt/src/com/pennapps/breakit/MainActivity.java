20aa73878912a92834c64c0c79ef55544f036d54
Waqas Leef <wikkii@msn.com>
2013-09-06 23:59:37 -0400

    grid backend

--- breakIt/src/com/pennapps/breakit/MainActivity.java
+++ breakIt/src/com/pennapps/breakit/MainActivity.java
@@ -3,6 +3,7 @@ package com.pennapps.breakit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		GridView gv = (GridView) findViewById(R.id.gridView);
+		gv.setAdapter(new ImageAdapter(this));
+		
 	}
 
 	@Override

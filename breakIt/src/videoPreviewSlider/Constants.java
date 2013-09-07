7789c9e9be1037836b01146199bf9151a4015399
Waqas Leef <wikkii@msn.com>
2013-09-06 22:12:45 -0400

    constans, activity_login

---
+++ breakIt/src/videoPreviewSlider/Constants.java
@@ -0,0 +1,13 @@
+package videoPreviewSlider;
+
+public class Constants {
+
+	public static final int Num_of_columns = 2;
+	
+	public static final int Grid = 6;
+	
+	public static String Video_Preview;
+	
+		
+	
+}

369c762c9c15c343bdfc40c9174921519731b42c
Waqas Leef <wikkii@msn.com>
2013-09-07 19:36:26 -0400

    details activity in progresss

--- breakIt/src/com/pennapps/breakit/DetailsActivity.java
+++ breakIt/src/com/pennapps/breakit/DetailsActivity.java
@@ -1,16 +1,50 @@
 package com.pennapps.breakit;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class DetailsActivity extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_details);
+		
 	}
-
+	
+	public class UploadVideo extends Activity {
+		private static final int SELECT_VIDEO = 2;
+		String selectedPath = "";
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_details);
+			
+			openGalleryVideo();
+		}
+		public void openGalleryVideo(){
+			Intent intent = new Intent();
+			intent.setType("video/*");
+			intent.setAction(Intent.ACTION_GET_CONTENT);
+			startActivityForResult(Intent.createChooser(intent,  "Select Video "), SELECT_VIDEO);
+		}
+		public void onActivityResult(int requestCode, int resultCode, Intent data){
+			if (resultCode == RESULT_OK){
+				if (requestCode == SELECT_VIDEO)
+				{
+					Uri selectedImageUri = data.getData();
+					selectedPath = getPath()
+				}
+			}
+		}
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.

6f7d304389cd7850468de029ff02563697d42685
Dan Silver <dannysilver3@gmail.com>
2013-09-08 05:11:57 -0500

    upload to main screen

--- breakIt/gen/com/pennapps/breakit/R.java
+++ breakIt/gen/com/pennapps/breakit/R.java
@@ -31,10 +31,11 @@ public final class R {
     }
     public static final class id {
         public static final int aboutTxt=0x7f08000b;
-        public static final int action_settings=0x7f08000d;
+        public static final int action_settings=0x7f08000f;
         public static final int breakButton=0x7f080008;
         public static final int buRecord=0x7f080005;
         public static final int buUpload=0x7f080003;
+        public static final int button1=0x7f08000e;
         public static final int editText1=0x7f080001;
         public static final int iconImg=0x7f080009;
         public static final int imgLogo=0x7f080006;
@@ -43,6 +44,7 @@ public final class R {
         public static final int sitesList=0x7f080004;
         public static final int textView1=0x7f080000;
         public static final int uploadProgress=0x7f080002;
+        public static final int upload_popup=0x7f08000d;
         public static final int video=0x7f080007;
     }
     public static final class layout {
@@ -51,6 +53,7 @@ public final class R {
         public static final int activity_splash=0x7f030002;
         public static final int activity_video_player=0x7f030003;
         public static final int row_site=0x7f030004;
+        public static final int upload_popup=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;

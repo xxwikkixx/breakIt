149c50c7ed039c86b9608c2685dab374dcffa7a2
Waqas Leef <wikkii@msn.com>
2013-09-08 04:18:51 -0400

    activity details number removed

--- breakIt/gen/com/pennapps/breakit/R.java
+++ breakIt/gen/com/pennapps/breakit/R.java
@@ -30,34 +30,28 @@ public final class R {
         public static final int stopbutton=0x7f020006;
     }
     public static final class id {
-        public static final int aboutTxt=0x7f080010;
-        public static final int action_settings=0x7f080012;
-        public static final int breakButton=0x7f08000c;
-        public static final int buRecord=0x7f080009;
-        public static final int buUpload=0x7f080007;
-        public static final int button1=0x7f08000d;
-        public static final int editDescription=0x7f080006;
-        public static final int editText1=0x7f080004;
-        public static final int iconImg=0x7f08000e;
-        public static final int imgLogo=0x7f08000a;
-        public static final int nameTxt=0x7f080011;
-        public static final int progress=0x7f08000f;
-        public static final int recordbutton=0x7f080001;
-        public static final int sitesList=0x7f080008;
-        public static final int textDesc=0x7f080005;
-        public static final int textVidDir=0x7f080003;
-        public static final int textView1=0x7f080002;
-        public static final int video=0x7f08000b;
-        public static final int videoView1=0x7f080000;
+        public static final int aboutTxt=0x7f08000c;
+        public static final int action_settings=0x7f08000e;
+        public static final int breakButton=0x7f080009;
+        public static final int buRecord=0x7f080006;
+        public static final int buUpload=0x7f080004;
+        public static final int editDescription=0x7f080003;
+        public static final int editText1=0x7f080001;
+        public static final int iconImg=0x7f08000a;
+        public static final int imgLogo=0x7f080007;
+        public static final int nameTxt=0x7f08000d;
+        public static final int progress=0x7f08000b;
+        public static final int sitesList=0x7f080005;
+        public static final int textDesc=0x7f080002;
+        public static final int textView1=0x7f080000;
+        public static final int video=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_camera=0x7f030000;
-        public static final int activity_details=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_splash=0x7f030003;
-        public static final int activity_video_player=0x7f030004;
-        public static final int mediaplayer=0x7f030005;
-        public static final int row_site=0x7f030006;
+        public static final int activity_details=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_splash=0x7f030002;
+        public static final int activity_video_player=0x7f030003;
+        public static final int row_site=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;

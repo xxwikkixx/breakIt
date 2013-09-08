27ddea1a61b70062c2ed53b8c5fba7b828a94020
Dan Silver <dannysilver3@gmail.com>
2013-09-07 20:11:53 -0500

    Merge

--- breakIt/src/com/pennapps/breakit/DetailsActivity.java
+++ breakIt/src/com/pennapps/breakit/DetailsActivity.java
@@ -15,6 +15,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.pennapps.breakit.MainActivity;
 
@@ -25,111 +27,18 @@ public class DetailsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_details);
 		
+		Button buUpload = (Button) findViewById(R.id.buUpload);
+		buUpload.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 	
-	public class UploadVideo extends Activity {
-		private static final int SELECT_VIDEO = 2;
-		String selectedPath = "";
-		
-		@Override
-		public void onCreate(Bundle savedInstanceState)
-		{
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_details);
-			
-			openGalleryVideo();
-		}
-		public void openGalleryVideo(){
-			Intent intent = new Intent();
-			intent.setType("video/*");
-			intent.setAction(Intent.ACTION_GET_CONTENT);
-			startActivityForResult(Intent.createChooser(intent,  "Select Video "), SELECT_VIDEO);
-		}
-		public void onActivityResult(int requestCode, int resultCode, Intent data){
-			if (resultCode == RESULT_OK){
-				if (requestCode == SELECT_VIDEO)
-				{
-					Uri selectedImageUri = data.getData();
-			//		selectedPath = getPath();//need a path here 
-					doFileUpload(); //file uploading
-				}
-			}
-		}
-		
-		private void doFileUpload(){
-			HttpURLConnection conn  = null;
-			DataOutputStream dos = null;
-			DataInputStream inStream = null;
-			String lineEnd = "rn";
-			String twoHyphens = "--";
-			String boundary = "*****";
-			int bytesRead, bytesAvailable, bufferSize;
-			byte[] buffer;
-			int maxBufferSize = 1*1024*1024;
-			String responseFromServer = "";
-			String urlString = "http://breakit.herokuapp.com/upload";
-			
-			try {
-				FileInputStream fileInputStream = new FileInputStream(new File(selectedPath));
-				URL url = new URL(urlString);
-				conn = (HttpURLConnection) url.openConnection();
-				conn.setDoInput(true);
-				conn.setDoOutput(true);
-				conn.setUseCaches(false);
-				conn.setRequestMethod("POST");
-				conn.setRequestProperty("Connection", "Keep-Alive");
-				conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
-		         dos = new DataOutputStream( conn.getOutputStream() );
-		         dos.writeBytes(twoHyphens + boundary + lineEnd);
-		         dos.writeBytes(lineEnd);
-		         // create a buffer of maximum size
-		         bytesAvailable = fileInputStream.available();
-		         bufferSize = Math.min(bytesAvailable, maxBufferSize);
-		         buffer = new byte[bufferSize];
-		         // read file and write it into form...
-		         bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-		         while (bytesRead > 0)
-		         {
-		          dos.write(buffer, 0, bufferSize);
-		          bytesAvailable = fileInputStream.available();
-		          bufferSize = Math.min(bytesAvailable, maxBufferSize);
-		          bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-		         }
-		         // send multipart form data necesssary after file data...
-		         dos.writeBytes(lineEnd);
-		         dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-		         // close streams
-		         Log.e("Debug","File is written");
-		         fileInputStream.close();
-		         dos.flush();
-		         dos.close();
-		        }
-		        catch (MalformedURLException ex)
-		        {
-		             Log.e("Debug", "error: " + ex.getMessage(), ex);
-		        }
-		        catch (IOException ioe)
-		        {
-		             Log.e("Debug", "error: " + ioe.getMessage(), ioe);
-		        }
-		        //------------------ read the SERVER RESPONSE
-		        try {
-		              inStream = new DataInputStream ( conn.getInputStream() );
-		              String str;
-		 
-		              while (( str = inStream.readLine()) != null)
-		              {
-		                   Log.e("Debug","Server Response "+str);
-		              }
-		              inStream.close();
-		 
-		        }
-		        catch (IOException ioex){
-		             Log.e("Debug", "error: " + ioex.getMessage(), ioex);
-		        }
-			}
-		}
-	//}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -137,5 +46,4 @@ public class DetailsActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
